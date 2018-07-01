c094d0ff0d0efbb2b8ee8abd9658a4a05bbf483a
Kjell Denti <kjelldg@gmail.com>
2018-07-01 20:18:57 +0200

    Compatible with more browsers now.

--- Javascripts/colorGame.js
+++ Javascripts/colorGame.js
@@ -48,26 +48,26 @@ resetButton.addEventListener("click", function () {
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.background = "steelblue";
+    h1.style.backgroundColor = "steelblue";
     messageDisplay.textContent = "";
     this.textContent = "New Colors";
 })
 colorDisplay.textContent = correctColor;
 
 for (var i = 0; i < squares.length; i++) {
-    squares[i].style.background = colors[i];
+    squares[i].style.backgroundColor = colors[i];
 
     squares[i].addEventListener("click", function() {
 
-      var clickedColor = this.style.background;
+      var clickedColor = this.style.backgroundColor;
       
       if (clickedColor === correctColor) {
           messageDisplay.textContent = "Correct!";
           changeColors(clickedColor);
-          h1.style.background = correctColor;
+          h1.style.backgroundColor = correctColor;
           resetButton.textContent = "Play Again?";
       } else {
-          this.style.background = "#232323";
+          this.style.backgroundColor = "#232323";
           messageDisplay.textContent = "Try again";
       }
         
